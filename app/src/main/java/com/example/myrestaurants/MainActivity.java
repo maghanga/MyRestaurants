b5348ef8af725c9cbff2397fb363360995087460
thinknehru <thinknehru@gmail.com>
2020-01-28 07:03:59 +0300

    Gather passed in data as Extra and create a new Relative layout for RestaurantsActivity

--- app/src/main/java/com/example/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/myrestaurants/MainActivity.java
@@ -2,11 +2,9 @@ package com.example.myrestaurants;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mLocationEditText = (EditText) findViewById(R.id.locationEditText);
@@ -25,12 +24,17 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
+
                 String location = mLocationEditText.getText().toString();
-                Log.d(TAG, location);
-                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+
+//                Log.d(TAG, location);
+
+                    Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                    intent.putExtra("location", location);
                 startActivity(intent);
 
-                Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show();
+//                Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show();
+
             }
         });
     }
--- app/src/main/java/com/example/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/example/myrestaurants/RestaurantsActivity.java
@@ -1,14 +1,18 @@
 package com.example.myrestaurants;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class RestaurantsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
     }
 }
