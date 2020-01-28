548e5a6232173ce08452c595c6f459acc98b48ea
thinknehru <thinknehru@gmail.com>
2020-01-28 10:52:08 +0300

    Hardcode restaurants into an array and use an ArrayAdapter to display them in a listview

--- app/src/main/java/com/example/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/example/myrestaurants/RestaurantsActivity.java
@@ -2,17 +2,44 @@ package com.example.myrestaurants;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RestaurantsActivity extends AppCompatActivity {
 
+    private String[] restaurants = new String[] {"The Collective", "Brew Bistro", "Burrito Bar", "Petma", "Ranalo's foods", "Kaldis", "Kilimanjaro", "Mama Rocks", "Nyama Mama", "Subway","KFC", "Chicken Inn", "Pepino's", "Teriyaki", "Charlie's Bisto"};
+
+    public static final String TAG = RestaurantsActivity.class.getSimpleName();
+    private TextView mLocationTextView;
+    private ListView mListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+
+        mListView = (ListView) findViewById(R.id.listView);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurants);
+        mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String restaurant = ((TextView)view).getText().toString();
+                Toast.makeText(RestaurantsActivity.this, restaurant, Toast.LENGTH_LONG).show();
+            }
+        });
+
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
+        mLocationTextView.setText("Here are all the restaurants near: " + location);
     }
 }
