4abf4172105e012b1d0baafe16bb781966435494
thinknehru <thinknehru@gmail.com>
2020-01-28 11:29:09 +0300

    Add butterknife to main activity to locate views

--- app/src/main/java/com/example/myrestaurants/MainActivity.java
+++ app/src/main/java/com/example/myrestaurants/MainActivity.java
@@ -5,36 +5,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG = MainActivity.class.getSimpleName();
-    private Button mFindRestaurantsButton;
-    private EditText mLocationEditText;
+    @BindView(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
+    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView)
+    TextView mAppNameTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
-        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+        ButterKnife.bind(this);
 
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 String location = mLocationEditText.getText().toString();
-
-//                Log.d(TAG, location);
-
-                    Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
-                    intent.putExtra("location", location);
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                intent.putExtra("location", location);
                 startActivity(intent);
-
-//                Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show();
-
             }
         });
     }
