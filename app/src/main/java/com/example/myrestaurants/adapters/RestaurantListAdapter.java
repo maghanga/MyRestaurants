9d43f73cd4c07892fa9cc8db5d37ae7a3e6044b4
thinknehru <thinknehru@gmail.com>
2020-02-04 10:59:07 +0300

    Add the new RestaurantPagerAdapter to the RestaurantDetailActivity

--- app/src/main/java/com/example/myrestaurants/adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/example/myrestaurants/adapters/RestaurantListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.myrestaurants.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.myrestaurants.R;
 import com.example.myrestaurants.models.Business;
 import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -46,7 +49,7 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
         return mRestaurants.size();
     }
 
-    public class RestaurantViewHolder extends RecyclerView.ViewHolder {
+    public class RestaurantViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         @BindView(R.id.restaurantImageView)
         ImageView mRestaurantImageView;
         @BindView(R.id.restaurantNameTextView) TextView mNameTextView;
@@ -56,12 +59,23 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
 
         private Context context;
 
+
         public RestaurantViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
             mContext = itemView.getContext();
+            itemView.setOnClickListener(this);
+
         }
 
+        @Override
+        public void onClick(View v) {
+            int itemPosition = getLayoutPosition();
+            Intent intent = new Intent(mContext, RestaurantDetailActivity.class);
+            intent.putExtra("position", itemPosition);
+            intent.putExtra("restaurants", Parcels.wrap(mRestaurants));
+            mContext.startActivity(intent);
+        }
 
         public void bindRestaurant(Business restaurant) {
             Picasso.get().load(restaurant.getImageUrl()).into(mRestaurantImageView);
--- app/src/main/java/com/example/myrestaurants/adapters/RestaurantPagerAdapter.java
+++ app/src/main/java/com/example/myrestaurants/adapters/RestaurantPagerAdapter.java
@@ -1,4 +1,34 @@
 package com.example.myrestaurants.adapters;
 
-public class RestaurantPagerAdapter {
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.myrestaurants.models.Business;
+import com.example.myrestaurants.ui.RestaurantDetailFragment;
+
+import java.util.List;
+
+public class RestaurantPagerAdapter extends FragmentPagerAdapter {
+    private List<Business> mRestaurants;
+
+    public RestaurantPagerAdapter(FragmentManager fm, int behavior, List<Business> restaurants) {
+        super(fm, behavior);
+        mRestaurants = restaurants;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return RestaurantDetailFragment.newInstance(mRestaurants.get(position));
+    }
+
+    @Override
+    public int getCount() {
+        return mRestaurants.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mRestaurants.get(position).getName();
+    }
 }
