f15080cd6042c5a0bcf1f33c542d5f42049e5fe7
thinknehru <thinknehru@gmail.com>
2020-02-13 10:08:36 +0300

    Create a login activity

---
+++ app/src/main/java/com/example/myrestaurants/ui/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.myrestaurants.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myrestaurants.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
